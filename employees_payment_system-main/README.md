132e1cae8fb93739b7f3aacf12ead5f37db540c2
Henrique Santiago Pires <88721828+santiagohenrique@users.noreply.github.com>
2024-04-19 10:15:01 -0300

    Add files via upload

---
+++ employees_payment_system-main/README.md
@@ -0,0 +1,18 @@
+# Sistema de pagamento de empregados
+
+## Descrição
+
+- Este é um projeto Java que demonstra um sistema simples de pagamento de funcionários. Os usuários podem inserir dados para um número específico de funcionários, incluindo nome, horas trabalhadas e valor por hora. Além disso, o usuário pode especificar se o funcionário é terceirizado, caso em que será incluído um encargo adicional no cálculo do pagamento.
+
+## Características
+
+- Coleta informações dos funcionários, incluindo nome, horas trabalhadas e valor por hora.
+- Permite que os usuários especifiquem se um funcionário é terceirizado e insiram uma cobrança adicional para funcionários terceirizados.
+- Calcula e exibe o pagamento de cada funcionário.
+- Utiliza conceitos de programação orientada a objetos, incluindo herança e upcasting, definindo duas classes: Employee e OutsourcedEmployee.
+
+## Como executar o programa
+- Clone ou baixe este projeto para sua máquina local.
+- Compile o código Java usando seu compilador Java preferido.
+- Execute o aplicativo compilado.
+- Siga as instruções na tela para inserir dados do funcionário.
---
+++ employees_payment_system-main/src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<Employee> employees = new ArrayList<>();
+		
+		System.out.print("Enter the number of employees: ");
+		int numberOfEmployees = sc.nextInt();
+
+		for(int i = 0; i < numberOfEmployees; i++) {
+			
+			System.out.println("Employee #" + (i + 1) + " data:");
+			System.out.print("Outsourced (y/n)? ");
+			char answer = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			if(answer == 'y') {
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				Employee employee = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				employees.add(employee);
+			}else {
+				Employee employee = new Employee(name, hours, valuePerHour);
+				employees.add(employee);
+			}
+			
+		}
+		
+		System.out.println("PAYMENTS:");
+		for(Employee employee : employees) {
+			System.out.println(employee.getName() + " - $ " + employee.payment());
+		}
+		
+
+	}
+	
+	
+}
